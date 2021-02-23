cc4c17ab484f9f7493086060fd28813884e2856c
GilbertoASJ <alvesgilberto84@gmail.com>
2021-02-22 22:52:05 -0300

    :airplane: Initial commit

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,12 @@
+export function ExperieneBar() {
+	return {
+		<header className="experience-bar">
+			<span>8 xp</span>
+			<div>
+				<div style={{ width: '60%' }} />
+				<span className="current-experience" style={{left: '50%'}}>300xp/span>
+			</div>
+			<span>600 xp</span>
+		</header>	
+	}
+}
